315785d94543b8a4d7df8ce81cc6d6528bc768e1
xjbeta <xjbeta.tiny@gmail.com>
2018-09-30 09:05:25 +0800

    Initial Commit

---
+++ Art Book/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Art Book
+//
+//  Created by xjbeta on 2018/9/30.
+//  Copyright © 2018 xjbeta. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
