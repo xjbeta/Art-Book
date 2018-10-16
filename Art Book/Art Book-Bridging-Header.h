70f6a8bee4e60e8d2e109a23613aa163626ac409
xjbeta <xjbeta.tiny@gmail.com>
2018-10-16 19:55:50 +0800

    Remove header import.

--- Art Book/Art Book-Bridging-Header.h
+++ Art Book/Art Book-Bridging-Header.h
@@ -2,11 +2,3 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-
-//#import "IKImageWrapper.h"
-//#import "IKRenderer.h"
-//#import "IKImageBrowserLayoutManager.h"
-//#import "IKImageBrowserGridGroup.h"
-//#import <Quartz/Quartz.h>
-
-//#import "IKImageBrowserLayoutManager.h"
