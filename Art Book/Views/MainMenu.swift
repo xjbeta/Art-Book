443f01e74aef62e9f1bd26c147f19490ebf6cde2
xjbeta <xjbeta.tiny@gmail.com>
2021-02-23 18:09:46 +0800

    MainMenu.

---
+++ Art Book/Views/MainMenu.swift
@@ -0,0 +1,19 @@
+//
+//  MainMenu.swift
+//  Art Book
+//
+//  Created by xjbeta on 2020/2/27.
+//  Copyright © 2020 xjbeta. All rights reserved.
+//
+
+import Cocoa
+
+class MainMenu: NSObject, NSMenuItemValidation {
+    func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
+        return true
+    }
+    
+    
+    
+
+}
